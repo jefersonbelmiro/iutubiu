93a3bd0c787516fd31c632a0dcd9ef328eb61254
Jeferson Belmiro <jeferson.belmiro@cwi.com.br>
2025-10-04 00:11:50 -0300

    fix: action

--- index.js
+++ index.js
@@ -4,33 +4,33 @@ const config = require("./config.json")
 const download_directory = config.download_directory
 
 if (!fs.existsSync(download_directory)) {
-    fs.mkdirSync(download_directory, { recursive: true })
+  fs.mkdirSync(download_directory, { recursive: true })
 }
 
 for (let index = 0; index < config.urls.length; index++) {
-    const url = create_url(config.urls[index])
-    console.log(`downloading ${(index + 1)}/${config.urls.length}`)
-    execute(config, url)
+  const url = create_url(config.urls[index])
+  console.log(`downloading ${(index + 1)}/${config.urls.length}`)
+  execute(config, url)
 }
 
 console.log('done')
 
 function execute(config, url) {
-    const downlod_path = fs.realpathSync(config.download_directory)
-    const binary = config?.binary || `docker run --rm -v "${downlod_path}:/downloads:rw" jauderho/yt-dlp:latest`
-    const args = config?.binary_args || `--audio-quality 0 --extract-audio --audio-format "${config.audio_format}" -f "bestaudio" -o "%(title)s.%(ext)s"`
-    const bin_with_args = `${binary} ${args} "${url}"`
-    try {
-        execSync(bin_with_args, { stdio: [] })
-    } catch (err) {
-        console.log(`ERROR on url: ${url}:\n ${err.stderr}`)
-    }
+  const downlod_path = fs.realpathSync(config.download_directory)
+  const binary = config?.binary || `docker run --rm -v "${downlod_path}:/downloads:rw" jauderho/yt-dlp:latest`
+  const args = config?.binary_args || `--audio-quality 0 --extract-audio --audio-format "${config.audio_format}" -f "bestaudio" -o "%(title)s.%(ext)s"`
+  const bin_with_args = `${binary} ${args} "${url}"`
+  try {
+    execSync(bin_with_args, { stdio: [] })
+  } catch (err) {
+    console.log(`ERROR on url: ${url}:\n ${err.stderr}`)
+    process.exit(1);
+  }
 }
 
-
 function create_url(url) {
-    if (!String(url).startsWith("http")) {
-        url = `https://www.youtube.com/watch?v=${url}`
-    }
-    return url
+  if (!String(url).startsWith("http")) {
+    url = `https://www.youtube.com/watch?v=${url}`
+  }
+  return url
 }
